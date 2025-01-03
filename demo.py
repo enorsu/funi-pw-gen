21fa2aa71de3b4049d2b562784c932b292ebeeb8
SotaNorsu <pelaaja538@gmail.com>
2025-01-03 19:17:32 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,12 @@
 import base64, sys
 
 obf = sys.argv[1]
-obf = "Y21WMGRYSnVJR2x1ZENnblR6QW5LUT09"
+obf = """
+i = 0
+while True:
+    i += 1
+    messagebox.showerror(__file__, "a" * i)
+"""
 obf = base64.b64encode(obf.encode()).decode()
 
 deobf = base64.b64decode(obf.encode()).decode()
