c6fc918db108c89bc77c735b52d36a949ab3d0f2
SotaNorsu <pelaaja538@gmail.com>
2025-01-03 14:05:42 +0200

    no idea

--- gen.py
+++ gen.py
@@ -10,18 +10,61 @@ def get_random_words(amount):
     words = json.loads(response.text)
     return words # thansk you copilot for the code
 
+
+def passwordGenerator(amt_words, numb_size):
+    words = get_random_words(amount=amt_words)
+    password = ""
+    for word in words:
+        password += word.capitalize()
+    password += str(random.randint(0, numb_size))
+    return password
+
+
+def passwordGeneratorV2(amt_tings):
+
+    formatter_keywords = ["num", "str"]
+    formatter = []
+    words = get_random_words(amount=amt_tings) # yeah yeah its wastes bandwidth but i dont care
+    result = ""
+
+    for i in range(amt_tings):
+        formatter.append(random.choice(formatter_keywords))
+    counter = 0
+    for type in formatter:
+        counter += 1
+        if type == "num":
+            result += str(random.randint(0, 9))
+        elif type == "str":
+            result += words[counter - 1].capitalize()
+
+    return result
+    
+
+
 def run_program_with_args():
-    args = sys.argv()
+    args = sys.argv
     first_args = ["--help", "--password", "--wizard"]
+
+    if len(args) < 2:
+        print("Please specify a command\nUse --help to see the commands")
+        return
+
     if args[1] in first_args:
         if args[1] == "--help":
             print("--wizard: Generates a password with a wizard")
             print("--password: Generates a password")
             print("--help: Shows the available commands")
         elif args[1] == "--password":
-            if (not args[2].isnumeric() ):
-                print("Invalid number")
+            try:
+                if (args[2].isnumeric()):
+                    args[2] = int(args[2])
+                    #print(passwordGenerator(amt_words=args[2], numb_size=args[2] * 10))
+                    print(passwordGeneratorV2(amt_tings=args[2]))
+                    return
+            except IndexError:
+                print("ERROR: Usage --password <amount>")
                 return
+
             
         elif args[1] == "--wizard":
             print("Generating a password with a wizard")
@@ -29,3 +72,5 @@ def run_program_with_args():
     else:
         print("Invalid command")
         print("Use --help to see the available commands")
+
+run_program_with_args()
---
+++ readme.md
@@ -0,0 +1,14 @@
+# password generator
+
+Usage:
+
+Clone repo
+
+`python gen.py <args>`
+
+`python gen.py --help`
+
+yeah i made this for fun
+(
+    includes funny comments
+)
